bddf7d6c702514415242f6b3164f27e06cf9dca8
Francisco Sandoval <francisco@mtblc.co>
2019-10-10 02:57:33 -0300

    wip - add ios MapBox (native and js)

---
+++ ios/MapBoxView.h
@@ -0,0 +1,12 @@
+//
+//  MapBoxView.h
+//  NativeComponentExample
+//
+//  Created by Francisco Sandoval on 10-10-19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <React/RCTView.h>
+
+@interface MapBoxView : RCTView
+@end
